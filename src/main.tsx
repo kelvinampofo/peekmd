87c1ff1f9f2f127993f83dcf7114c72482159bfa
Kelvin Ampofo <kelvnamp@gmail.com>
2026-09-01 23:58:52 +0100

    Restore app styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import MarkdownPreviewer from "./features/markdown-preview/MarkdownPreviewer";
-
 import "./styles/system.css";
+
+import MarkdownPreviewer from "./features/markdown-preview/MarkdownPreviewer";
 
 const root = document.getElementById("root");
 
